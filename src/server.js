3a59019aedf80a0c63bb4d85db2219fa2706e4f8
Mark Anthony <89271406+Marksva@users.noreply.github.com>
2025-07-02 20:11:54 -0300

    fix: refactor server to handle request streams and parse JSON body

--- src/server.js
+++ src/server.js
@@ -10,25 +10,40 @@ import http from 'node:http';
 // stateless - stateless é quando o servidor não guarda o estado do usuário
 
 const users = [];
- 
-const server = http.createServer((req, res) => {
+import { randomUUID } from 'node:crypto';
 
+const server = http.createServer(async (req, res) => {
     const { method, url } = req;
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
 
     if (method === 'GET' && url === '/users') {
         return res
-            .setHeader('Content-Type', 'application/json')
-            .end(JSON.stringify(users));
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+       
         users.push({
-            id: 1,
-            name: 'Mark',
-            email: 'mark@example.com',
+            id: randomUUID(),
+            name,
+            email,
         })
 
-        return res.writeHead(201).end();
+        return res.writeHead(201).end()
     }
 
     return res.writeHead(404).end(JSON.stringify({
