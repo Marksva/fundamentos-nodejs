62dffee71d7395e0e4e0f9119de4ae1da85b09df
Mark Anthony <89271406+Marksva@users.noreply.github.com>
2025-07-02 20:21:04 -0300

    fix: add comments to clarify stream handling and buffer usage

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -13,6 +13,13 @@ class InverseNumberStream extends Transform {
 
 // req => ReadableStream
 // res => WritableStream
+// buffers => o buffer é um espaço de memória que armazena os dados temporariamente
+// TransformStream => é um tipo de stream que transforma os dados que estão sendo lidos
+// os chunks sao os pedaços de dados que estão sendo lidos do stream
+// vamos utilizar chunks e streams para ler dados que não cabem na memória de uma vez só
+// como por exemplo um arquivo muito grande ou uma requisição muito grande
+// musicas ou videos que são muito grandes
+// o stream é uma forma de ler e escrever dados de forma eficiente, sem precisar carregar tudo
 
 const server = http.createServer(async (req, res) => {
     const buffers = []
